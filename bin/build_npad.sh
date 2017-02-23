8f4bcc7403a2e0f9aff0f0ac36e668d28d0ee934
Greg Russell <gfr10598@gmail.com>
2017-02-23 22:38:14 +0000

    point to gfr10598

--- bin/build_npad.sh
+++ bin/build_npad.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-yum install -y man sudo
 cd ~/builder
-./build_one.sh https://github.com/m-lab/npad-support master iupui_npad
+./build_one.sh https://github.com/gfr10598/npad-support 1.5.7.pre1 iupui_npad
 
 # When this exits, the rpm should be available under slicebase-i386/i686
 
